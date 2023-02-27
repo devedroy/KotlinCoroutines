495dc60873087eee8ce3d85b274e70ed69cd22e7
Deved Roy <devedroy.dr@gmail.com>
2023-02-27 14:28:26 +0530

    Global Scope and delay
    delay doesn't stop other coroutines in the thread.
    POV: Thread is the whole construction site and Coroutine is just a worker in it.

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            delay(5000L)
+            Log.d(TAG, "Coroutines says hello from ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Thread says hello from ${Thread.currentThread().name}")
     }
 }
